c8cd4bd1066db61c1a6242ed19b11607f8506803
Inocxh <s204453@student.dtu.dk>
2023-04-11 12:31:13 +0200

    undid weird commit

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/inocxh/Dokumenter/DTU/6.semester/bachelor/top-trees
-# Build directory: /home/inocxh/Dokumenter/DTU/6.semester/bachelor/top-trees
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/inocxh/Dokumenter/DTU/6.semester/bachelor/top-trees/tests_include-b12d07c.cmake")
-subdirs("src/lib/Catch2")
--- tests_include-b12d07c.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/inocxh/Dokumenter/DTU/6.semester/bachelor/top-trees/tests_tests-b12d07c.cmake")
-  include("/home/inocxh/Dokumenter/DTU/6.semester/bachelor/top-trees/tests_tests-b12d07c.cmake")
-else()
-  add_test(tests_NOT_BUILT-b12d07c tests_NOT_BUILT-b12d07c)
-endif()
