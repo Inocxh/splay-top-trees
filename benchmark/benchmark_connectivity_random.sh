2de9b1bcc0e955a0ee9afe5b67e23a87c1b06844
Magnus Siegumfeldt <m.ks@outlook.dk>
2024-06-05 15:52:43 +0200

    table plot

--- benchmark/benchmark_connectivity_random.sh
+++ benchmark/benchmark_connectivity_random.sh
@@ -9,8 +9,8 @@ ITERATIONS=3
 
 if [ -f $INPUT_FILE ]
 then
-    mkdir -p results
-    rm -f results/$DATA_FILE
+    mkdir -p results/splay_top_tree/
+    rm -f results/splay_top_tree/$DATA_FILE
     
     for n in 500 1000 2000 3000 4000 5000 6000 7000 8000
     do
@@ -20,7 +20,7 @@ then
             echo "Set $i" 
             for _ in $(eval echo {1..$REPEAT})
             do
-                ./../build/benchmark_connectivity $WARMUPS $ITERATIONS dataset/connectivity/connec_${n}_${i}.txt >> results/$DATA_FILE || exit
+                ./../build/benchmark_connectivity $WARMUPS $ITERATIONS dataset/connectivity/connec_${n}_${i}.txt >> results/splay_top_tree/$DATA_FILE || exit
             done
         done
     done
